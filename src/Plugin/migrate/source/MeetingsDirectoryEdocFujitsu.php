a49f1b246d629daca722c80c372ac423590b6148
Stanislav Kutasevits <stan@bellcom.dk>
2021-11-01 15:45:54 +0200

    First commit

---
+++ src/Plugin/migrate/source/MeetingsDirectoryEdocFujitsu.php
@@ -0,0 +1,366 @@
+<?php
+
+namespace Drupal\os2web_meetings_edoc_fujitsu\Plugin\migrate\source;
+
+use Drupal\Component\FileSystem\RegexDirectoryIterator;
+use Drupal\migrate\Event\MigrateImportEvent;
+use Drupal\migrate\Plugin\MigrationInterface;
+use Drupal\node\Entity\Node;
+use Drupal\os2web_meetings\Entity\Meeting;
+use Drupal\os2web_meetings\Form\SettingsForm;
+use Drupal\os2web_meetings\Plugin\migrate\source\MeetingsDirectory;
+use Drupal\migrate\Row;
+
+/**
+ * Source plugin for retrieving data via URLs.
+ *
+ * @MigrateSource(
+ *   id = "os2web_meetings_directory_edoc_fujitsu"
+ * )
+ */
+class MeetingsDirectoryEdocFujitsu extends MeetingsDirectory {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration) {
+    $urls = $this->collectAgendaUrls($configuration);
+    $configuration['urls'] = $urls;
+
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $migration);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function collectAgendaUrls($configuration) {
+    $manifestPaths = [];
+    $agendaPaths = [];
+
+    // Traverse through the directory (not recursively)
+    $path = $this->getMeetingsManifestPath();
+    $iterator = new RegexDirectoryIterator($path, $configuration['pattern']);
+    foreach ($iterator as $fileinfo) {
+      $manifestPaths[] = $fileinfo->getPathname();
+    }
+
+    // Loading agenda XML's.
+    foreach ($manifestPaths as $manifest) {
+      $manifalestRealPath = \Drupal::service('file_system')->realpath($manifest);
+
+      libxml_clear_errors();
+      $manifestXml = simplexml_load_file($manifalestRealPath);
+      foreach (libxml_get_errors() as $error) {
+        $error_string = self::parseLibXmlError($error);
+        \Drupal::logger('os2web_meetings')->error('URL skipped. XML invalid syntax: ' . $error_string);
+        return FALSE;
+      }
+
+      // Returned as array with one element.
+      $isPublish = $manifestXml->xpath('/Root/edoc:Notification/edoc:Publish');
+      $isPublish = (string) array_shift($isPublish);
+
+      if (filter_var($isPublish, FILTER_VALIDATE_BOOLEAN)) {
+        // Returned as array with one element.
+        $agendaPath = $manifestXml->xpath('/Root/edoc:Notification/edoc:PathToXml');
+        $agendaPath = (string) array_shift($agendaPath);
+        $agendaPath = $this->invertPathsBackslashes($agendaPath);
+
+        $agendaPaths[] = $this->getMeetingsManifestPath() . $agendaPath;
+      }
+    }
+
+    return $agendaPaths;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function prepareRow(Row $row) {
+    $source = $row->getSource();
+
+    // Altering the title.
+    $dateParts = explode(' ', $source['meeting_start_date']);
+
+    // New title will be [Meeting title - DD/mm/YYYY]
+    $title = $source['title'] . ' - ' . $dateParts[0];
+
+    $row->setSourceProperty('title', $title);
+
+    return parent::prepareRow($row);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getMeetingsManifestPath() {
+    return \Drupal::config(SettingsForm::$configName)
+      ->get('edoc_fujitsu_meetings_manifest_path');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertAgendaIdToCanonical(array $source) {
+    return $source['agenda_id'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertAgendaAccessToCanonical(array $source) {
+    return filter_var($source['agenda_access'], FILTER_VALIDATE_BOOLEAN);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertAgendaTypeToCanonical(array $source) {
+    if (strcasecmp($source['agenda_type'], 'Agenda') === 0) {
+      return MeetingsDirectory::AGENDA_TYPE_DAGSORDEN;
+    }
+    else {
+      return MeetingsDirectory::AGENDA_TYPE_REFERAT;
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertStartDateToCanonical(array $source) {
+    $start_date = $source['meeting_start_date'];
+
+    return $this->convertDateToTimestamp($start_date);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertEndDateToCanonical(array $source) {
+    $end_date = $source['meeting_end_date'];
+
+    return $this->convertDateToTimestamp($end_date);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertAgendaDocumentToCanonical(array $source) {
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertCommitteeToCanonical(array $source) {
+    $id = $source['committee_id'];
+    $name = $source['committee_name'];
+    return [
+      'id' => $id,
+      'name' => $name,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertLocationToCanonical(array $source) {
+    $id = $source['location_name'];
+    $name = $source['location_name'];
+    return [
+      'id' => $id,
+      'name' => $name,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertBulletPointsToCanonical(array $source) {
+    $canonical_bullet_points = [];
+    $source_bullet_points = $source['bullet_points'];
+
+    foreach ($source_bullet_points as $bullet_point) {
+      $id = $bullet_point['Identifier'];
+      $bpNumber = $bullet_point['HandlingItem']['SerialNo'];
+      $title = $bullet_point['HandlingItem']['Title'];
+
+      // Skipping bullet points with no titles.
+      if (!$title) {
+        continue;
+      }
+
+      print_r($title);
+      print_r(PHP_EOL);
+      $publishingType = $bullet_point['AccessIsPublic'];
+      $access = filter_var($publishingType, FILTER_VALIDATE_BOOLEAN);
+
+      // Getting attachments (text).
+      $source_attachments = $bullet_point['HandlingItem']['CasePresentations'];
+      $canonical_attachments = [];
+      if (is_array($source_attachments) && array_key_exists('CasePresentation', $source_attachments)) {
+        $canonical_attachments = $this->convertAttachmentsToCanonical($source_attachments['CasePresentation']);
+      }
+
+      // Getting enclosures (files).
+      $source_enclosures = $bullet_point['HandlingItem']['Attachments'];
+      $canonical_enclosures = [];
+      if (is_array($source_enclosures) && array_key_exists('Attachment', $source_enclosures)) {
+        $canonical_enclosures = $this->convertEnclosuresToCanonical($source_enclosures);
+      }
+
+      $canonical_bullet_points[] = [
+        'id' => $id,
+        'number' => $bpNumber,
+        'title' => $title,
+        'access' => $access,
+        'attachments' => $canonical_attachments,
+        'enclosures' =>  $canonical_enclosures,
+      ];
+    }
+
+    usort($canonical_bullet_points, function ($item1, $item2) {
+    if ($item1['number'] == $item2['number']) return 0;
+    return $item1['number'] < $item2['number'] ? -1 : 1;
+    });
+
+    return $canonical_bullet_points;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertAttachmentsToCanonical(array $source_attachments, $access = TRUE) {
+    $canonical_attachments = [];
+
+    foreach ($source_attachments as $source_attachment) {
+      // Using title as ID, as we don't have a real one.
+      $id = $source_attachment['Title'];
+
+      if (!empty($source_attachment['Content'])) {
+        $canonical_attachments[] = [
+          'id' => $id,
+          'title' => $source_attachment['Title'],
+          'body' => $source_attachment['Content'],
+          'access' => TRUE,
+        ];
+      }
+    }
+
+    return $canonical_attachments;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function convertEnclosuresToCanonical(array $source_enclosures) {
+    $canonical_enclosures = [];
+
+    foreach ($source_enclosures as $enclosure) {
+      $id = $enclosure['Identifier'];
+      $title = $enclosure['Title'];
+      $access = TRUE;
+      $uri = $enclosure['Link'];
+      $uri = $this->invertPathsBackslashes($uri);
+
+      $canonical_enclosures[] = [
+        'id' => $id,
+        'title' => $title,
+        'uri' => $uri,
+        'access' => $access,
+      ];
+
+      print_r($canonical_enclosures);
+    }
+
+    return $canonical_enclosures;
+  }
+
+  /**
+   * Converts Danish specific string date into timestamp in UTC.
+   *
+   * @param string $dateStr
+   *   Date as string.
+   *
+   * @return int
+   *   Timestamp in UTC.
+   *
+   * @throws \Exception
+   */
+  private function convertDateToTimestamp($dateStr) {
+    $dateTime = new \DateTime($dateStr, new \DateTimeZone('Europe/Copenhagen'));
+
+    return $dateTime->getTimestamp();
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function convertParticipantToCanonical(array $source) {
+    return '';
+  }
+
+  /**
+   * Replacing backslash with normal slash.
+   */
+
+  /**
+   * Replacing backslash with normal slash.
+   *
+   * @param $path
+   *   Path string
+   *
+   * @return string|string[]
+   *   String with the backslashes being inverted.
+   */
+  private function invertPathsBackslashes($path) {
+    return str_replace('\\', '/', $path);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function postImport(MigrateImportEvent $event) {
+    parent::postImport($event);
+
+    // Find all meetings.
+    $query = \Drupal::entityQuery('node');
+    $query->condition('type', 'os2web_meetings_meeting');
+    $query->condition('field_os2web_m_source', $this->getPluginId());
+    $entity_ids = $query->execute();
+
+    $meetings = Node::loadMultiple($entity_ids);
+
+    // Group meetings as:
+    // $groupedMeetings[<meeting_id>][<agenda_id>] = <node_id> .
+    $groupedMeetings = [];
+    foreach ($meetings as $meeting) {
+      $os2webMeeting = new Meeting($meeting);
+
+      $meeting_id = $os2webMeeting->getMeetingId();
+      $agenda_id = $os2webMeeting->getEsdhId();
+
+      $groupedMeetings[$meeting_id][$agenda_id] = $os2webMeeting->id();
+
+      // Sorting agendas, so that lowest agenda ID is always the first.
+      sort($groupedMeetings[$meeting_id]);
+    }
+
+    // Process grouped meetings and set addendum fields.
+    foreach ($groupedMeetings as $meeting_id => $agendas) {
+      // Skipping if agenda count is 1.
+      if (count($agendas) == 1) {
+        continue;
+      }
+
+      $mainAgendaNodedId = array_shift($agendas);
+
+      foreach ($agendas as $agenda_id => $node_id) {
+        // Getting the meeting.
+        $os2webMeeting = new Meeting($meetings[$node_id]);
+
+        // Setting addendum field, meeting is saved inside a function.
+        $os2webMeeting->setAddendum($mainAgendaNodedId);
+      }
+    }
+  }
+}
