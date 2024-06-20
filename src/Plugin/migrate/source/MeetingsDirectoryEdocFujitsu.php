39934c6cf5a4dd98b5a62703fec526c97409fb40
Henrik Tholle <ht@bellcom.dk>
2024-06-20 09:33:26 +0200

    Fix accessCheck on entityQuery

--- src/Plugin/migrate/source/MeetingsDirectoryEdocFujitsu.php
+++ src/Plugin/migrate/source/MeetingsDirectoryEdocFujitsu.php
@@ -368,7 +368,7 @@ class MeetingsDirectoryEdocFujitsu extends MeetingsDirectory {
     parent::postImport($event);
 
     // Find all meetings.
-    $query = \Drupal::entityQuery('node');
+    $query = \Drupal::entityQuery('node')->accessCheck(false);
     $query->condition('type', 'os2web_meetings_meeting');
     $query->condition('field_os2web_m_source', $this->getPluginId());
     $entity_ids = $query->execute();
